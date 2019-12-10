67c32f27587b7307ce823229717df192e6c61d88
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-12-10 14:29:44 -0800

    add logic to ask for up to 10 guesses per game

--- index.js
+++ index.js
@@ -7,18 +7,26 @@ var position = Math.floor(Math.random() * 5);
 var word = new Word(wordsToGuess[position]);
 console.log(word.toString());
 
-// word.checkChar("a");
-// console.log(word.toString());
-inquirer
-    .prompt({
-        type: "input",
-        name: "guess",
-        message: "Guess a Letter",
-    })
-    .then(function (response) {
-        word.checkChar(response.guess)
-        console.log(word.toString())
-    });
+guessRound();
+var remainingGuesses = 10;
+
+function guessRound() {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "guess",
+            message: "Guess a Letter",
+        })
+        .then(function (response) {
+            word.checkChar(response.guess)
+            console.log(word.toString())
+
+            if (remainingGuesses <= 10) {
+                guessRound();
+                remainingGuesses--;
+            }
+        });
+};
 
 
 // wordsToGuess[0].Word();
